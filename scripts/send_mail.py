09d457617f1650b74a9158248e322ef066eedc4f
snowmelt9696 <3031573339@qq.com>
2026-06-21 13:04:23 +0800

    fix: correct Steam挂刀 fields - use buff_steam_* not steam_buff_*

--- scripts/send_mail.py
+++ scripts/send_mail.py
@@ -125,7 +125,7 @@ def generate_item_section(data):
     s += "<div class=\"st\">📊 平台对比</div><div class=\"pg2\">" + pcs + "</div>"
     s += "<div class=\"st\">📈 价格趋势</div><div class=\"tw\"><table class=\"tt\"><thead><tr><th>周期</th><th>涨跌金额</th><th>涨跌幅</th></tr></thead><tbody>" + trs + "</tbody></table></div>"
     # Steam/Buff conversion
-    cv1 = str(i.get("steam_buff_buy_conversion", "-")); cv2 = str(i.get("buff_steam_sell_conversion", "-"))
+    cv1 = str(i.get("buff_steam_buy_conversion", "-")); cv2 = str(i.get("buff_steam_sell_conversion", "-"))
     s += "<div class=\"st\">🔄 Steam挂刀参考</div><table class=\"ct\"><tr><td><div class=\"lbl\">Steam求购挂刀</div><div class=\"val\">" + cv1 + "</div></td><td><div class=\"lbl\">Steam在售挂刀</div><div class=\"val\">" + cv2 + "</div></td></tr></table>"
     s += "</div></div>"
     return s
